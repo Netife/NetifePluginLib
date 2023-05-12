30d7309274a4f9653c67ec09a128279ef7b22391
EpicMo <1982742309@qq.com>
2023-05-12 10:19:28 +0800

    parserRawHtml

--- NetifeBasicLib.cpp
+++ NetifeBasicLib.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "NetifeBasicLib.h"
+#include "TextHelper.h"
+#include <Poco/JSON/Parser.h>
 
 NetifeBasicLib::~NetifeBasicLib() {}
 
@@ -14,13 +16,10 @@ std::string NetifeBasicLib::GetVersion() const{
     return "1.0.0";
 }
 
-std::string NetifeBasicLib::DispatcherCommand(std::string commandOriginal) {
-    agent->LogInfo("NetifeBasicLib is commanded by " + commandOriginal);
-    return "over";
-}
-
 void NetifeBasicLib::OnLoaded() {
-    agent->LogInfo("NetifeBasicLib loaded success!!!");
+    agent->LogInfo("NetifeBasicLib loaded success!");
+    agent->LogInfo("NetifeBasicLib is a basic component of Netife, it is updated without bound of Netife Primary Component, please "
+                   "check update at ");
 }
 
 bool NetifeBasicLib::OnEnable() {
@@ -33,6 +32,40 @@ bool NetifeBasicLib::OnDisable() {
 
 void NetifeBasicLib::OnExiting() {
     agent->LogInfo("NetifeBasicLib is trying to clear the cache of shared lib...");
+}
+
+std::string
+NetifeBasicLib::DispatcherCommand(std::string commandHead, std::map<std::string, std::optional<std::string>> params) {
+    if (commandHead == "parserRawHtml"){
+        return ParserRawHtml(params["rawHtml"].value());
+    }
+
+    return "";
+}
+
+std::string NetifeBasicLib::ParserRawHtml(const string &rawHtml) {
+
+    vector<string> htmlArr = Netife::TextHelper::split(rawHtml, "\r\n\r\n");
+
+    Poco::JSON::Object json;
+
+    if (htmlArr.size() == 2){
+        json.set(std::string("dataBody"), htmlArr[1]);
+    }
+
+    vector<string> htmlHeadLine = Netife::TextHelper::split(htmlArr[0],"\r\n");
+
+    json.set(string("status"), htmlHeadLine[0]);
+
+    for (int i = 1; i < htmlHeadLine.size(); ++i) {
+        vector<string> temp = Netife::TextHelper::split(htmlHeadLine[i], ":");
+        json.set(string("head::") + temp[0],temp[1]);
+    }
+
+    std::ostringstream oss;
+    json.stringify(oss);
+
+    return oss.str();
 }
 
 POCO_BEGIN_MANIFEST(NetifePlugins)
--- NetifeBasicLib.h
+++ NetifeBasicLib.h
@@ -22,7 +22,7 @@ public:
 
     std::string GetVersion() const override;
 
-    std::string DispatcherCommand(std::string commandOriginal) override;
+    std::string DispatcherCommand(std::string commandHead, std::map<std::string, std::optional<std::string>> params) override;
 
     //当插件启用的时候 [顺序不明确]
     bool OnEnable() override;
@@ -35,5 +35,7 @@ public:
 
     //当软件即将退出的时候 [此时所有插件都即将关闭]
     void OnExiting() override;
+
+    std::string ParserRawHtml(const string &rawHtml);
 };
 #endif //NETIFEDEMOPLUGIN_DEMOCLASS_H
